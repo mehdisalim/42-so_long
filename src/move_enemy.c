36afc13f30b950b6f9b83ade749f6a13b1664f96
mehdisalim <mehdisalimofficial2000@gmail.com>
2022-12-15 21:54:49 +0100

    working on enemy move feature

--- src/move_enemy.c
+++ src/move_enemy.c
@@ -6,7 +6,7 @@
 /*   By: esalim <esalim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 18:56:15 by esalim            #+#    #+#             */
-/*   Updated: 2022/12/15 20:20:45 by esalim           ###   ########.fr       */
+/*   Updated: 2022/12/15 20:42:07 by esalim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,33 +21,19 @@ int move_enemy(t_data *mlx)
 
     lst = mlx->lists.enemy;
 	void	*img = mlx_xpm_file_to_image(mlx->ptr, "./assets/background/bg3.xpm", &a, &a);
-    if (x == 20000 && mlx->map[lst->content.x + 1][lst->content.y] == '0')
+    if (x == 4000 && mlx->map[lst->content.x][lst->content.y + 1] == '0')
     {
-		mlx_put_image_to_window(mlx->ptr, mlx->win, img, lst->content.x * 50, (lst->content.y * 50) + 50);
-        mlx->map[lst->content.x][lst->content.y] = '0';
-        mlx->map[lst->content.x + 1][lst->content.y] = 'X';
-        drawing_map(mlx);
+		mlx_put_image_to_window(mlx->ptr, mlx->win, img, lst->content.x * 50 - 50, lst->content.y * 50);
+        // mlx->map[lst->content.x][lst->content.y] = '0';
+        // mlx->map[lst->content.x][lst->content.y + 1] = 'X';
+		mlx_put_image_to_window(mlx->ptr, mlx->win, mlx->imgs.enemy.img0, lst->content.x * 50 - 50, lst->content.y * 50);
     }
-    if (x == 40000 && mlx->map[lst->content.x][lst->content.y + 1] == '0')
+    if (x == 8000 && mlx->map[lst->content.x][lst->content.y - 1] == '0')
     {
-		mlx_put_image_to_window(mlx->ptr, mlx->win, img, lst->content.x * 50, (lst->content.y * 50) + 50);
-        mlx->map[lst->content.x][lst->content.y] = '0';
-        mlx->map[lst->content.x][lst->content.y + 1] = 'X';
-        drawing_map(mlx);
-    }
-    if (x == 60000 && mlx->map[lst->content.x - 1][lst->content.y] == '0')
-    {
-		mlx_put_image_to_window(mlx->ptr, mlx->win, img, lst->content.x * 50, (lst->content.y * 50) + 50);
-        mlx->map[lst->content.x - 1][lst->content.y] = 'X';
-        mlx->map[lst->content.x][lst->content.y] = '0';
-        drawing_map(mlx);
-    }
-    if (x == 80000 && mlx->map[lst->content.x][lst->content.y - 1] == '0')
-    {
-		mlx_put_image_to_window(mlx->ptr, mlx->win, img, (lst->content.x) * 50, (lst->content.y + 2) * 50);
-        mlx->map[lst->content.x][lst->content.y - 1] = 'X';
-        mlx->map[lst->content.x][lst->content.y] = '0';
-        drawing_map(mlx);
+		mlx_put_image_to_window(mlx->ptr, mlx->win, img, (lst->content.x + 1) * 50, lst->content.y * 50);
+        // mlx->map[lst->content.x][lst->content.y - 1] = 'X';
+        // mlx->map[lst->content.x][lst->content.y] = '0';
+		mlx_put_image_to_window(mlx->ptr, mlx->win, mlx->imgs.enemy.img0, lst->content.x * 50 + 50, lst->content.y * 50);
         x = 0;
     }
     return 0;
