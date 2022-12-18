ab2a2ef099b6044cd13c36bd8a25eeee1799db9f
mehdisalim <mehdisalimofficial2000@gmail.com>
2022-12-18 21:40:43 +0100

    add rand() function to move_enemy.c

--- src/move_enemy.c
+++ src/move_enemy.c
@@ -6,7 +6,7 @@
 /*   By: esalim <esalim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 18:56:15 by esalim            #+#    #+#             */
-/*   Updated: 2022/12/18 14:00:46 by esalim           ###   ########.fr       */
+/*   Updated: 2022/12/18 21:37:58 by esalim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,8 +32,7 @@ void	enemy_to_left(t_data *mlx, int *x)
 		}
 		lst = lst->next;
 	}
-	if (*x >= 4000 * 8)
-		*x = 0;
+	*x = 0;
 }
 
 void	enemy_to_up(t_data *mlx, int *x)
@@ -41,7 +40,6 @@ void	enemy_to_up(t_data *mlx, int *x)
 	t_list	*lst;
 
 	lst = mlx->enemy;
-	(void)x;
 	while (lst)
 	{
 		if (mlx->map[lst->content.x][lst->content.y - 1] == 'P')
@@ -56,6 +54,7 @@ void	enemy_to_up(t_data *mlx, int *x)
 		}
 		lst = lst->next;
 	}
+	*x = rand() % 10000 + 1;
 }
 
 void	enemy_to_down(t_data *mlx, int *x)
@@ -63,7 +62,6 @@ void	enemy_to_down(t_data *mlx, int *x)
 	t_list	*lst;
 
 	lst = mlx->enemy;
-	(void)x;
 	while (lst)
 	{
 		if (mlx->map[lst->content.x][lst->content.y + 1] == 'P')
@@ -78,6 +76,7 @@ void	enemy_to_down(t_data *mlx, int *x)
 		}
 		lst = lst->next;
 	}
+	*x = rand() % 10000 + 1;
 }
 
 void	enemy_to_right(t_data *mlx, int *x)
@@ -85,7 +84,6 @@ void	enemy_to_right(t_data *mlx, int *x)
 	t_list	*lst;
 
 	lst = mlx->enemy;
-	(void)x;
 	while (lst)
 	{
 		if (mlx->map[lst->content.x + 1][lst->content.y] == 'P')
@@ -100,6 +98,7 @@ void	enemy_to_right(t_data *mlx, int *x)
 		}
 		lst = lst->next;
 	}
+	*x = rand() % 10000 + 1;
 }
 
 int	move_enemy(t_data *mlx)
@@ -108,13 +107,13 @@ int	move_enemy(t_data *mlx)
 
 	x++;
 	loop_hook(mlx);
-	if (x == 4000 || x == 4000 * 7)
+	if (x == 2800)
 		enemy_to_up(mlx, &x);
-	else if (x == 4000 * 2 || x == 4000 * 6)
+	else if (x == 5600)
 		enemy_to_right(mlx, &x);
-	else if (x == 4000 * 3 || x == 4000 * 5)
+	else if (x == 8400)
 		enemy_to_down(mlx, &x);
-	else if (x == 4000 * 4 || x == 4000 * 8)
+	else if (x > 8400)
 		enemy_to_left(mlx, &x);
 	return (0);
 }
